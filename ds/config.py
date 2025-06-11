7f1587295cf6330865e7a0d68011cb7bddb5ee53
Nicola C. <nx9889@gmail.com>
2025-06-11 11:35:26 +0200

    fix: permanently disabled DeepFilterNet as implementation is broken

--- ds/config.py
+++ ds/config.py
@@ -6,5 +6,7 @@ ORIGINS = (getenv("DS_ORIGINS") or "*").split(",")
 # Boosts the volume before transcribing, it may enhance or worsen the transcription
 BOOST_VOLUME = (getenv("DS_BOOST_VOLUME") or "false") == "true"
 
-ENABLE_DEEP_FILTER_NET = (getenv("DS_ENABLE_DEEP_FILTER_NET") or "false") == "true"
+# FIXME: permanently disabled until a working implementation is done
+ENABLE_DEEP_FILTER_NET = False and (getenv("DS_ENABLE_DEEP_FILTER_NET") or "false") == "true"
 
+ENABLE_AUDIO_DEBUG = (getenv("DS_ENABLE_AUDIO_DEBUG") or "false") == "true"
